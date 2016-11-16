17f701ec2b19e2f94bdd4f8ec77c6a09605c39ce
cassac <hicassac@gmail.com>
2016-11-15 23:30:56 -0800

    initial component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App = () => <div>Hello World</div>;
+export default class App extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
