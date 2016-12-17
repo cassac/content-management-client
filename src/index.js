7153d29bce78e80a64ad2a586db96590d02c89c1
cassac <hicassac@gmail.com>
2016-12-16 17:12:16 -0800

    connects user dashboard

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
+import UsersDashboard from './containers/UsersDashboard';
 
 render(
   <Router history={browserHistory}>
     <Route path='/dashboard' component={App}>
+      <Route path='/dashboard/users' component={UsersDashboard} />
     </Route>
   </Router>, 
   document.getElementById('app')
