26bd63cbdb84b5fd73dfb7098d09dfff7c4f5b79
cassac <hicassac@gmail.com>
2016-12-20 19:15:44 -0800

    adds new user modal

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Button, Glyphicon } from 'react-bootstrap';
+import { Button, Glyphicon, Modal } from 'react-bootstrap';
 
 const NewUser = () => {
   return (
-    <Button 
-      bsStyle="primary"
-    >
-      <Glyphicon glyph="plus" />
-       Create new user
-    </Button>
+    <div>
+      <Button 
+        bsStyle="primary"
+        bsSize="large"
+        onClick={ () => {console.log('new user')} }
+      >
+        <Glyphicon glyph="plus" />
+        Create new user
+      </Button>
+      <Modal show={true} onHide={ () => {console.log('hide modal')} }>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Create a new user</h4>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+          <hr />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={ () => { console.log('hide modal button')} }>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   )
 }
 
