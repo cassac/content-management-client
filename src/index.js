07f2cb2a40fc83f35b1c2e2e15bdb52804a15270
cassac <hicassac@gmail.com>
2016-12-20 19:57:06 -0800

    connects redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import App from './components/App';
 import UsersDashboard from './containers/UsersDashboard';
 
+const store = persistStore(configureStore());
+
 render(
-  <Router history={browserHistory}>
-    <Route path='/dashboard' component={App}>
-      <Route path='/dashboard/users' component={UsersDashboard} />
-    </Route>
-  </Router>, 
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/dashboard' component={App}>
+        <Route path='/dashboard/users' component={UsersDashboard} />
+      </Route>
+    </Router>
+  </Provider>, 
   document.getElementById('app')
 );
