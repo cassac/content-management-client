eb48ec64affcf577dd325d868dbd505db3a3eac0
cassac <hicassac@gmail.com>
2016-12-27 13:58:57 -0800

    adds modal toggling to reducer. utilizes object spread syntax

--- src/modules/users/reducers/users.js
+++ src/modules/users/reducers/users.js
@@ -4,15 +4,22 @@ const initialState = {
   success: null,
   message: null,
   results: [],
+  newUserModalOpen: false,
   error: null
 }
 
 const users = (state = initialState, action) => {
   switch (action.type) {
     case types.REQUESTED_USERS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         results: action.results
-      });
+      };
+    case types.NEW_USER_MODAL_OPEN:
+      return {
+        ...state,
+        newUserModalOpen: !state.newUserModalOpen
+      };
     default:
       return state;
   }
