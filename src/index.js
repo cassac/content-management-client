f028ff07848b3df7b1bc63dbc150dd7934c83ea6
cassac <hicassac@gmail.com>
2016-12-27 22:21:05 -0800

    auto rehydrate triggers two rerenders?

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { persistStore } from 'redux-persist';
+// import { persistStore } from 'redux-persist';
 import storeConfig from './reducers';
 import App from './modules/common/components/App';
 import UsersDashboard from './modules/users/components/Dashboard';
 
 const store = storeConfig();
-persistStore(store);
+// persistStore(store);
 
 render(
   <Provider store={store}>
