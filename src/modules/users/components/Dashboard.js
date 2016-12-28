67d6482f314860fb82d607db84942833f0a6ae47
cassac <hicassac@gmail.com>
2016-12-27 22:21:44 -0800

    adds user list

--- src/modules/users/components/Dashboard.js
+++ src/modules/users/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserBar from '../components/UserBar';
+import UserList from '../components/UserList';
 
 export default class UsersDashboard extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class UsersDashboard extends Component {
       <div>
         <h1>Users Dashboard</h1>
         <UserBar />
+        <UserList />
       </div>
     )
   }
