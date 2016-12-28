906a87900d069aa303ee92ed6eb46b1bbc931c42
cassac <hicassac@gmail.com>
2016-12-27 21:42:11 -0800

    connects store state and dispatch

--- src/modules/users/components/SortBar.js
+++ src/modules/users/components/SortBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { sortUsersBy } from '../actions';
 import { userSearchCriteria } from '../../../config.js';
 import { Button, Glyphicon, Grid, Row, Col } from 'react-bootstrap';
@@ -12,18 +13,18 @@ class SortBar extends Component {
           <Col xs={2} key={item}> 
             <Row className="show-grid" className="text-center">
               <Col xs={12} > 
-                {item}
+                <strong>{item}</strong>
               </Col>
               <Col xs={12} className="text-center" > 
                 <Button
                   bsSize="xsmall" 
-                  onClick={ () => {console.log('Up')} }
+                  onClick={ () => this.props.onSortClick(item, 'ascending') }
                 >
                   <Glyphicon glyph="arrow-up" />
                 </Button>
                 <Button
                   bsSize="xsmall" 
-                  onClick={ ()=> {console.log('Down')} }
+                  onClick={ ()=> this.props.onSortClick(item, 'descending') }
                 >
                   <Glyphicon glyph="arrow-down" />
                 </Button>
@@ -41,4 +42,19 @@ class SortBar extends Component {
   }
 }
 
-export default SortBar;
+
+const mapStateToProps = (state) => {
+  return {
+    sortUsers: state.sortUsers,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSortClick: (criteria, direction) => {
+      dispatch(sortUsersBy(criteria, direction));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SortBar);
