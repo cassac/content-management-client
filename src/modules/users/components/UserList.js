d4479e5c80ebc27c64973824fb13736ba5b3d86f
cassac <hicassac@gmail.com>
2016-12-28 15:57:19 -0800

    adds event listeners for custom stling

--- src/modules/users/components/UserList.js
+++ src/modules/users/components/UserList.js
@@ -4,11 +4,24 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import UserListItem from './UserListItem';
 
 class UserList extends Component {
+  componentDidMount() {
+    const listItems = document.querySelectorAll('.userListItem');
+    listItems.forEach(item => {
+      item.addEventListener('mouseenter', e => {
+        e.target.classList.add('hoverItem');
+        e.target.lastChild.classList.remove('showOnHover');
+      });
+      item.addEventListener('mouseleave', e => {
+        e.target.classList.remove('hoverItem');
+        e.target.lastChild.classList.add('showOnHover');
+      });
+    });
+  }
   renderUsers() {
     return this.props.users.map(user => (
         <UserListItem 
           key={user._id}
-          user={user} 
+          user={user}
         />
     ));
   }
