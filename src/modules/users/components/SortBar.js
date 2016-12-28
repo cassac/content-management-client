3f8213f020302634299e87ba28ae4992a0699d0b
cassac <hicassac@gmail.com>
2016-12-27 21:18:15 -0800

    sort bar styling

--- src/modules/users/components/SortBar.js
+++ src/modules/users/components/SortBar.js
@@ -9,7 +9,26 @@ class SortBar extends Component {
     const createSortToggles = () => {
       return userSearchCriteria.map(item => {
         return (
-          <Col xs={2} > {item}
+          <Col xs={2} key={item}> 
+            <Row className="show-grid" className="text-center">
+              <Col xs={12} > 
+                {item}
+              </Col>
+              <Col xs={12} className="text-center" > 
+                <Button
+                  bsSize="xsmall" 
+                  onClick={ () => {console.log('Up')} }
+                >
+                  <Glyphicon glyph="arrow-up" />
+                </Button>
+                <Button
+                  bsSize="xsmall" 
+                  onClick={ ()=> {console.log('Down')} }
+                >
+                  <Glyphicon glyph="arrow-down" />
+                </Button>
+              </Col>
+            </Row>
           </Col>
         )
       });
