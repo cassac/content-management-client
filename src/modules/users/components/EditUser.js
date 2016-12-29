9a522386de9813c5073db5ffe9e6d4f9278bb16b
cassac <hicassac@gmail.com>
2016-12-29 11:29:51 -0800

    adds edit user component, connects to store

---
+++ src/modules/users/components/EditUser.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { toggleEditUserModal } from '../actions';
+import { Button, Glyphicon, Modal } from 'react-bootstrap';
+
+class EditUser extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        <Glyphicon 
+          glyph="pencil" 
+          title={`Edit ${user.username}'s profile`}
+          onClick={ ()=>this.props.onModalClick(user._id) }
+        />
+        <Modal 
+          show={this.props.users.editUserModalOpen} 
+          onHide={ ()=>this.props.onModalClick(user._id) }
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Edit user</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+            <hr />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={ ()=>this.props.onModalClick(user._id) }>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onModalClick: (userId) => {
+      dispatch(toggleEditUserModal(userId));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditUser);
