c6b93edf95e261add07e55bd2c6328d3a22270e6
cassac <hicassac@gmail.com>
2016-12-29 10:32:33 -0800

    switch to stateless component

--- src/modules/users/components/Dashboard.js
+++ src/modules/users/components/Dashboard.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserBar from '../components/UserBar';
 import UserList from '../components/UserList';
 
-export default class UsersDashboard extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Users Dashboard</h1>
-        <UserBar />
-        <UserList />
-      </div>
-    )
-  }
+const UsersDashboard = () => {
+  return (
+    <div>
+      <h1>Users Dashboard</h1>
+      <UserBar />
+      <UserList />
+    </div>
+  )
 }
+
+export default UsersDashboard;
