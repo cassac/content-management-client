d5182a05d61325db5a1cfe65cda83525ba7f1c5c
cassac <hicassac@gmail.com>
2017-01-02 19:26:23 -0800

    adds create user form inputs

---
+++ src/modules/users/components/CreateUserFormInputs.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Control, Form } from 'react-redux-form';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+const CreateUserFormInputs = () => {
+  return (
+    <Grid style={{width: 'inherit'}}>
+    <Row>
+      <Col xs={6} className='text-center'>
+        <img 
+          src="https://placehold.it/100x100" 
+          className='img-responsive img-rounded'
+          style={{
+            margin: '0 auto',
+            'marginBottom': '38px'
+          }}
+        />
+        <br/>
+        <label className='pull-left'>Profile Photo</label>
+        <input type='file' className='form-control'/>
+      </Col>
+      <Col xs={6}>
+        <Row className="show-grid">
+          <Col>
+            <label>Username</label>
+            <Control.text 
+              model="createUser.username" 
+              className='form-control'
+            />
+          <br/>
+          </Col>
+          <Col>
+            <label>Email</label>
+            <Control.text 
+              model="createUser.email" 
+              className='form-control'
+            />
+          <br/>
+          </Col>
+          <Col>
+            <label>Company</label>
+            <Control.text 
+              model="createUser.company" 
+              className='form-control'
+            />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+    </Grid>
+  )
+}
+
+export default CreateUserFormInputs;
