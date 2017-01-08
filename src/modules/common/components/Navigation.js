fd88ccd21688f958d06104fda46493b8053c7d07
cassac <hicassac@gmail.com>
2017-01-07 17:54:07 -0800

    refactors navigation into dummy component

--- src/modules/common/components/Navigation.js
+++ src/modules/common/components/Navigation.js
@@ -1,62 +1,45 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-class Navigation extends Component {
-  renderAuth() {
-    if (this.props.auth.authenticated) {
+const Navigation = ({authenticated}) => {
+  const renderAuth = () => {
+    if (authenticated) {
       return (
-        <Nav pullRight>
-          <LinkContainer to='/dashboard/users'>
-            <NavItem>Users</NavItem>
-          </LinkContainer>
-          <LinkContainer to='/dashboard/files'>
-            <NavItem>Files</NavItem>
-          </LinkContainer>
-          <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown">
-            <LinkContainer to='/dashboard/profile'>
-              <MenuItem eventKey={3.1}>Profile</MenuItem>
-            </LinkContainer>
-            <MenuItem divider />
-            <LinkContainer to='/dashboard/signout'>
-              <MenuItem eventKey={3.2}>Signout</MenuItem>
-            </LinkContainer>
-          </NavDropdown>          
-        </Nav>
-      )
-    }
-    else {
-      return (
-        <Nav pullRight>
-          <LinkContainer to='/dashboard/signin'>
-            <NavItem>Sign In</NavItem>
-          </LinkContainer>          
-        </Nav>
-      )
-    }
-  }
-  render() {
-    return (
-      <Navbar collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            Logo
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
         <Navbar.Collapse>
-          { this.renderAuth() }
+          <Nav pullRight>
+            <LinkContainer to='/dashboard/users'>
+              <NavItem>Users</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/dashboard/files'>
+              <NavItem>Files</NavItem>
+            </LinkContainer>
+            <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown">
+              <LinkContainer to='/dashboard/profile'>
+                <MenuItem eventKey={3.1}>Profile</MenuItem>
+              </LinkContainer>
+              <MenuItem divider />
+              <LinkContainer to='/dashboard/signout'>
+                <MenuItem eventKey={3.2}>Signout</MenuItem>
+              </LinkContainer>
+            </NavDropdown>          
+          </Nav>
         </Navbar.Collapse>
-      </Navbar>
-    )
+      )
+    }
+    return;
   }
+  return (
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          Logo
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      { renderAuth() }
+    </Navbar>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    auth: state.auth.authenticated
-  }
-}
-
-export default connect(mapStateToProps)(Navigation);
+export default Navigation;
