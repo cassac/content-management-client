7372a8e198a4c859a0cad4bc07aeb1e616d49422
cassac <hicassac@gmail.com>
2017-01-07 20:00:01 -0800

    connects action creators, action types and dispatch

--- src/modules/auth/actions.js
+++ src/modules/auth/actions.js
@@ -1,14 +1,23 @@
+import { browserHistory } from 'react-router';
 import { axios } from '../../config';
-import types from './actionTypes';
+import types from './actionTypes' ;
+import { 
+  handleRequestSuccess, 
+  handleRequestError, 
+  handleRequestFail, 
+} from '../common/actions';
 
-export default SignInRequest = (credentials) => {
+export const signInRequest = (credentials) => {
   return dispatch => {
     axios.post(`auth/signin`, credentials)
     .then(response => {
-      // dispatch
+      localStorage.setItem('token', response.data.token);
+      dispatch({ type: types.USER_SIGNIN });
+      dispatch(handleRequestSuccess(response));
+      browserHistory.push('/dashboard/users');
     })
     .catch(error => {
-      // dispatch
+      dispatch(handleRequestError(error));
     })
   }
 }
