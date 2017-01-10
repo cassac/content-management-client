b4eb02b31e95871f496acef6f95faa3445959efb
cassac <hicassac@gmail.com>
2017-01-10 00:19:10 -0800

    extends axios config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 axios.defaults.headers.common['authorization'] = localStorage.token;
+// `validateStatus` defines whether to resolve or reject the promise for a given
+// HTTP response status code. If `validateStatus` returns `true` (or is set to `null`
+// or `undefined`), the promise will be resolved; otherwise, the promise will be
+// rejected.
+axios.defaults.validateStatus = function (status) {
+  return status >= 200 && status < 500; 
+}
 
 module.exports = {
   axios,
