429f34f30e92ed1fd3be2ec61c0bc1fd1ad805e8
cassac <hicassac@gmail.com>
2017-01-12 16:14:08 -0800

    refactor layout

--- src/modules/users/components/EditUserFormInputs.js
+++ src/modules/users/components/EditUserFormInputs.js
@@ -1,59 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Control, Form } from 'react-redux-form';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Checkbox } from 'react-bootstrap';
 import FlashMessage from '../../common/components/FlashMessage';
 
-const EditUserFormInputs = ({users, renderErrors}) => {
-  return (
-     <Grid style={{width: 'inherit'}}>
-      <Row>
-        <Col xs={6} className='text-center'>
-          <img 
-            src="https://placehold.it/100x100" 
-            className='img-responsive img-rounded'
-            style={{
-              margin: '0 auto',
-              'marginBottom': '38px'
-            }}
-          />
-          <br/>
-          <label className='pull-left'>Update Photo</label>
-          <input type='file' className='form-control'/>
-        </Col>
-        <Col xs={6}>
-          <Row className="show-grid">
-            <Col>
-              <label>Username</label>
-              <Control.text 
-                model="editUser.username" 
-                defaultValue={users.editUsername || ''}
-                className='form-control'
-              />
-            <br/>
-            </Col>
-            <Col>
-              <label>Email</label>
-              <Control.text 
-                model="editUser.email" 
-                defaultValue={users.editUserEmail || ''}
-                className='form-control'
-              />
-            <br/>
-            </Col>
-            <Col>
-              <label>Company</label>
-              <Control.text 
-                model="editUser.company" 
-                defaultValue={users.editUserCompany || ''}
-                className='form-control'
-              />
-              <FlashMessage />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Grid>
-  )
+class EditUserFormInputs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true
+    }
+  }
+  render() {
+    return (
+       <Grid style={{width: 'inherit'}}>
+        <Row className='text-center'>
+          <Col xs={6}>
+            <Row className="show-grid">
+              <Col>
+                <label>Username</label>
+                <Control.text 
+                  model="editUser.username" 
+                  defaultValue={this.props.users.editUsername || ''}
+                  className='form-control'
+                />
+              <br/>
+              </Col>
+              <Col>
+                <label>Email</label>
+                <Control.text 
+                  model="editUser.email" 
+                  defaultValue={this.props.users.editUserEmail || ''}
+                  className='form-control'
+                />
+              <br/>
+              </Col>
+              <Col>
+                <label>Company</label>
+                <Control.text 
+                  model="editUser.company" 
+                  defaultValue={this.props.users.editUserCompany || ''}
+                  className='form-control'
+                />
+                <FlashMessage />
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={6}>
+            <br />
+            <Checkbox 
+              inline={true}
+              onChange={()=> this.setState({disabled: !this.state.disabled })}
+            >
+            Check to update password
+            </Checkbox>
+            <br />
+            <label>New Password</label>
+            <Control 
+              model="editUser.password" 
+              className='form-control'
+              type='password'
+              disabled={this.state.disabled}
+            />
+            <label>Confirm New Password</label>
+            <Control 
+              model="editUser.confirmPassword" 
+              className='form-control'
+              type="password"
+              disabled={this.state.disabled}
+            />
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 }
 
 export default EditUserFormInputs;
