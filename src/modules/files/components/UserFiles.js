99cf98fe5633012d6a19942c48bdfa5e40edcd23
cassac <hicassac@gmail.com>
2017-01-17 16:13:11 -0800

    initializes files dashboard

--- src/modules/files/components/UserFiles.js
+++ src/modules/files/components/UserFiles.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import FlashMessage from '../../common/components/FlashMessage';
 
-export default class UserFiles extends Component {
-  render() {
-    const { userId } = this.props.params;
-    return (
-      <div>
-        {`User ${userId}'s Files`}
-      </div>
-    )
-  }
+const UserFiles = ({params}) => {
+  return (
+    <div>
+      <Grid>
+        <Row>
+          <Col xs={4}>
+            <h4>{`User ${params.username}'s Files`}</h4>
+          </Col>
+          <Col xs={4}>
+            <FlashMessage />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  )
 }
+
+export default UserFiles;
