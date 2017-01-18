d601c81899dbb1dce3634c858f954c58eb9fe044
cassac <hicassac@gmail.com>
2017-01-18 11:15:15 -0800

    creates files list component and connects store

---
+++ src/modules/files/components/FilesList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Grid } from 'react-bootstrap';
+import FilesListItem from './FilesListItem';
+
+import { toggleEditFileModal, toggleDeleteFileModal, getFiles } from '../actions';
+
+class FilesList extends Component {
+  render() {
+    return(
+      <Grid className='text-center'>
+        { this.props.files.map(file => (
+          <FilesListItem 
+            key={file._id}
+            file={file}
+            onEditFileModalClick={ () => this.props.onEditFileModalClick(file) }
+            onDeleteFileModalClick={ () => this.props.onDeleteFileModalClick(file) }
+          />
+        ))}
+      </Grid>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    files: state.files.results,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onEditFileModalClick: file => dispatch(toggleEditFileModal(file)),
+    onDeleteFileModalClick: file => dispatch(toggleDeleteFileModal(file)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilesList);
