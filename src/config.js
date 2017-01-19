e3e85eca9dc962718e5aef39bc1d53b38c140cb9
cassac <hicassac@gmail.com>
2017-01-18 18:10:22 -0800

    remove comment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 axios.defaults.headers.common['authorization'] = localStorage.token;
-// axios.defaults.headers.common['authorization'] = localStorage.token;
 // `validateStatus` defines whether to resolve or reject the promise for a given
 // HTTP response status code. If `validateStatus` returns `true` (or is set to `null`
 // or `undefined`), the promise will be resolved; otherwise, the promise will be
