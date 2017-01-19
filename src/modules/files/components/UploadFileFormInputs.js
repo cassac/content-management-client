a560a41784b4e29da41c8bd34e4a421a0278deb7
cassac <hicassac@gmail.com>
2017-01-18 16:34:15 -0800

    refactor form fields

--- src/modules/files/components/UploadFileFormInputs.js
+++ src/modules/files/components/UploadFileFormInputs.js
@@ -5,28 +5,28 @@ import FlashMessage from '../../common/components/FlashMessage';
 const UploadFileFormInputs = () => {
   return (
     <div>
-    <Grid style={{width: 'inherit'}}>
-      <Row>
-        <Col xs={6} className='text-center'>
-          <label className='pull-left'>Upload File</label>
-          <input type='file' className='form-control'/>
-        </Col>
-        <Col xs={6}>
-          <Row className="show-grid">
-            <Col>
-              <label>Comment</label>
-              <Control.text 
-                model="createUser.username" 
-                className='form-control'
-                required
-              />
-              <FlashMessage />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
+      <Grid style={{width: 'inherit'}}>
+        <Row>
+          <Col xs={6} className='text-center'>
+            <label className='pull-left'>Upload File</label>
+            <input type='file' className='form-control'/>
+          </Col>
+          <Col xs={6}>
+            <Row className="show-grid">
+              <Col>
+                <label>Comment</label>
+                <input
+                  type='text' 
+                  className='form-control'
+                  required
+                />
+                <FlashMessage />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
   )
 }
 
