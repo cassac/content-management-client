87b5199928acf7bdd892a642009951e8ac3753a5
cassac <hicassac@gmail.com>
2017-01-22 21:22:18 -0800

    refactored file get request to include token in query params

--- src/modules/files/components/FilesListItem.js
+++ src/modules/files/components/FilesListItem.js
@@ -18,7 +18,7 @@ const FileItem = ({file, onEditFileModalClick, onDeleteFileModalClick}) => {
     >
       <Col xs={2}>
         <p>
-          <a href={ `/api/users/${ file.ownerId }/files/${file._id }` }>
+          <a href={`/api/users/${ file.ownerId }/files/${file._id }?token=${localStorage.token}`} >
             {parseFilename(file.filePath)}
           </a>
         </p>
@@ -35,13 +35,13 @@ const FileItem = ({file, onEditFileModalClick, onDeleteFileModalClick}) => {
       <Col xs={4} className='showOnHover'>
         <Row className="show-grid">
           <Col xs={4}>
-            <Link to={`/api/users/${ file.ownerId }/files/${file._id }`}>
+            <a href={`/api/users/${ file.ownerId }/files/${file._id }?token=${localStorage.token}`} >
               <Glyphicon 
                 glyph="download-alt" 
                 title={`Download file ${parseFilename(file.filePath)}`}
                 onClick={ () => console.log(`Download file ${parseFilename(file.filePath)}`) }
               />
-            </Link>
+            </a>
           </Col>
           <Col xs={4}>
             <Glyphicon 
