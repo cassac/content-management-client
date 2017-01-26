2e20f9e4c179841a59cc6ed84683be4f1d109a6a
cassac <hicassac@gmail.com>
2017-01-25 19:01:29 -0800

    modal styling

--- src/modules/files/components/UploadFileFormInputs.js
+++ src/modules/files/components/UploadFileFormInputs.js
@@ -7,7 +7,7 @@ const UploadFileFormInputs = ({onChangeHandler}) => {
     <div>
       <Grid style={{width: 'inherit'}}>
         <Row>
-          <Col xs={6} className='text-center'>
+          <Col xs={12} className='text-center'>
             <label className='pull-left'>Upload File</label>
             <input 
               name='file'
@@ -16,20 +16,18 @@ const UploadFileFormInputs = ({onChangeHandler}) => {
               onChange={ ({target}) => onChangeHandler(target.name, target.files[0]) }
             />
           </Col>
-          <Col xs={6}>
-            <Row className="show-grid">
-              <Col>
-                <label>Comment</label>
-                <input
-                  name='comment'
-                  type='text' 
-                  className='form-control'
-                  required
-                  onChange={ ({target}) => onChangeHandler(target.name, target.value)  }
-                />
-                <FlashMessage />
-              </Col>
-            </Row>
+          <Col xs={12}>
+            <br />
+            <label>Comment</label>
+            <textarea
+              name='comment'
+              type='text' 
+              className='form-control'
+              required
+              onChange={ ({target}) => onChangeHandler(target.name, target.value)  }
+            >
+            </textarea>
+            <FlashMessage />
           </Col>
         </Row>
       </Grid>
